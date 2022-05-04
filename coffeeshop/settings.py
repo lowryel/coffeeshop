9e85bd217149ce06e89969e895f57f8e8c07f96b
lowryel <eugeneagbaglo@gmail.com>
2022-05-04 16:31:41 +0100

    a

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--=5yyc444(u6t&kqtbr_dgzdbv6)#o4*8gsd%&!nui2qw7^w3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['172.0.0.1', 'koffeeapp.herokuapp.com']
 
